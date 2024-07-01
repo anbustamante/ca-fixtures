6d7e7d78e235056f87cd1f21deaea3ef2be007ae
Andres Bustamante <andres.bustamante@softtekprojects.com>
2024-07-01 14:00:49 -0300

    Subida de proyecto en blanco

---
+++ src/main/java/com/club_app/fixture/FixtureApiApplication.java
@@ -0,0 +1,13 @@
+package com.club_app.fixture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixtureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixtureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/club_app/fixture/FixtureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club_app.fixture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixtureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
